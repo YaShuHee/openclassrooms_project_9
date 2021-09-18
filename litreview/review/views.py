3e19bf95fe64f7f2e4285fe131264f960f1d2c41
Timothé Gélibert <tim.gelibert@protonmail.com>
2021-09-18 12:26:10 +0200

    Corrected wrong DateTimeField

--- litreview/review/views.py
+++ litreview/review/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+def sign_up(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        # pass
+    return render(request, "review/base.html", {"connected": False, "content": "Feed page"})
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username, password)
+        if user is not None:
+            login(request, user)
+            return redirect("/feed/")
+
+
+def feed(request):
+    return render(request, "litreview/base.html", {"connected": True, "content": "Feed page"})
+
+
+def follow(request):
+    pass
+
+
+def ticket_creation(request):
+    pass
+
+
+def ticket_modification(request):
+    pass
+
+
+def review_creation(request):
+    pass
+
+
+def review_modification(request):
+    pass
